51193186f452a0ab8b99d78c966977b28516bf98
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-21 15:29:23 +0600

    added add money part

--- js/home.js
+++ js/home.js
@@ -1,1 +1,17 @@
-console.log('Home added')
+//console.log('Home added')
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+    const addMoneyInput=document.getElementById('add-money').value
+    const pinNumberInput = document.getElementById('pin-number-input').value;
+    if(pinNumberInput==='123')
+    {
+       
+        const balance= document.getElementById('current-blance').innerText;
+        const newBalance = parseFloat(balance)+parseFloat(addMoneyInput);
+        document.getElementById('current-blance').innerText=newBalance;
+    }
+    else{
+        alert('failed to add money please try again')
+    }
+    
+})
