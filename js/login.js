2ffb0f7f37435ca26f89f156c58fcd666a3d4129
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-21 14:14:07 +0600

    added home page partial

--- js/login.js
+++ js/login.js
@@ -1,6 +1,15 @@
 //console.log('login checked')
-document.getElementById('btn-login').addEventListener('click',function(){
-    console.log('button clicked')
+document.getElementById('btn-login').addEventListener('click',function(event){
+    event.preventDefault();
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById('pin-number').value;
+    if(phoneNumber==="5" && pinNumber==="123")
+    {
+        console.log('you are logged in');
+        // move to another page in same tab
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('wrong phone number or pin');
+    }
 })
