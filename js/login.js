32150c937e3a290cdb287e29e877871ce6fa2b8c
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-21 13:36:32 +0600

    added partial event handler

---
+++ js/login.js
@@ -0,0 +1,6 @@
+//console.log('login checked')
+document.getElementById('btn-login').addEventListener('click',function(){
+    console.log('button clicked')
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber)
+})
